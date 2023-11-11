6e514bada7f687b245beb477a445b3f6ad278c53
Murat Mutlu <muratmutlu010@gmail.com>
2023-11-11 19:05:51 +0300

    :sparkles:

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
-  .then(console.log('Connected to MongoDB!'))
+  .then(console.log('Connected to MongoDB!!'))
   .catch((err) => {
     console.log(err);
   });
